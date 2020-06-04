dc76fa2d0d658416bbbafc12e8bf9817db2a0f12
eastclo <goldeastcloud@gmail.com>
2020-06-04 19:33:11 +0900

    makefile 과 기본 헤더 선언

---
+++ Makefile
@@ -0,0 +1,15 @@
+all : ssu_crontab ssu_crond ssu_rsync
+
+ssu_crontab : ssu_crontab.c
+	gcc -o ssu_crontab ssu_crontab.c
+
+ssu_crond : ssu_crond.c
+	gcc -o ssu_crond ssu_crond.c
+
+ssu_rsync : ssu_rsync.c
+	gcc -o ssu_rsync ssu_rsync.c
+
+clean :
+	rm ssu_rsync
+	rm ssu_crond
+	rm ssu_crontab
---
+++ default.h
@@ -0,0 +1,31 @@
+#ifndef DEFAULT_H_
+#define DEFAULT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <syslog.h>
+#include <signal.h>
+
+#ifndef true
+	#define true 1
+#endif
+#ifndef false
+	#define false 0
+#endif
+#ifndef FILELEN 
+	#define FILELEN 256 
+#endif
+#ifndef BUFLEN
+	#define BUFLEN 1024
+#endif
+
+#endif
