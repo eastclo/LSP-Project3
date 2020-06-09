dd26c244cd93a120795ba62b5569818b04fed596
eastclo <goldeastcloud@gmail.com>
2020-06-09 18:25:24 +0900

    주기 오류시 에러

--- ssu_crontab.c
+++ ssu_crontab.c
@@ -1,6 +1,7 @@
 #include "default.h"
 #include "ssu_crontab.h"
 
+struct timeval begin_t, end_t;
 FILE *file_fp; //ssu_crontab_file
 FILE *log_fp; //ssu_crontab_log
 char crontabFile[FILELEN]; //ssu_crontab_file 경로
@@ -12,6 +13,12 @@ int main(void)
     char buf[BUFLEN];
 	char command[BUFLEN];
 	int fd;
+
+	gettimeofday(&begin_t, NULL); //프로그램 시작시간 측정
+	if(atexit(ssu_runtime)) {
+		fprintf(stderr, "atexit error\n");
+		exit(1);
+	}
 
 	//log파일과 ssu_crontab_file의 경로 저장
     getcwd(buf, FILELEN);
@@ -50,6 +57,20 @@ int main(void)
 	}
 
 	exit(0);
+}
+
+void ssu_runtime(void) //프로그램 수행시간 출력함수
+{
+	gettimeofday(&end_t, NULL); //프로그램 종료시간 측정
+    end_t.tv_sec -= begin_t.tv_sec;  //시작시간 초와 종료시간초의 차이 계산
+
+    if(end_t.tv_usec < begin_t.tv_usec){  //시작시과 종료시간 micro초차이 계산
+        end_t.tv_sec--;
+        end_t.tv_usec += SECOND_TO_MICRO;
+    }
+
+    end_t.tv_usec -= begin_t.tv_usec;
+    printf("Runtime: %ld:%06ld(sec:usec)\n", end_t.tv_sec % 60, end_t.tv_usec);
 }
 
 void print_prompt()
@@ -137,10 +158,149 @@ void cmd_add(char *cmd) //add명령어 실행
 	write_log(cmd, "add");
 }
 
-int check_add_cmd(char *cmd) //TODO:실행주기 오류시 false리턴
+int check_add_cmd(char *cmd) //실행주기 오류시 false리턴
 {
+	char tmp[BUFLEN];
+	char *ptr;
+	strcpy(tmp, cmd);
+
+	//분
+	if((ptr = strtok(tmp, " ")) == NULL)
+		return false;
+	if(check_time(0, 59, ptr) == false)
+		return false;
+
+	//시
+	if((ptr = strtok(NULL, " ")) == NULL)
+		return false;
+	if(check_time(0, 23, ptr) == false)
+		return false;
+
+	//일
+	if((ptr = strtok(NULL, " ")) == NULL)
+		return false;
+	if(check_time(1, 31, ptr) == false)
+		return false;
+
+	//월
+	if((ptr = strtok(NULL, " ")) == NULL)
+		return false;
+	if(check_time(1, 12, ptr) == false)
+		return false;
+
+	//요일
+	if((ptr = strtok(NULL, " ")) == NULL)
+		return false;
+	if(check_time(0, 6, ptr) == false)
+		return false;
+
+	//명령어가 없으면 에러
+	if((ptr = strtok(NULL, " ")) == NULL)
+		return false;
+
 	return true;
 }
+
+
+int check_cycle(int low, int high, char *cycle) //cycle이 low, high에 존재하고, 형식에 이상 없으면 true
+{
+	int buf[BUFLEN] = {0,};
+	int start, end;
+	int i;
+	int buflen = 0, lexlen;
+
+	for(i = 0; i < strlen(cycle); i++) {
+		lexlen = 0;
+		//숫자 일 때
+		if(isdigit(cycle[i])) { 
+			while(isdigit(cycle[i])) 
+				lexeme[lexlen++] = cycle[i++];
+			lexeme[lexlen] = 0;
+			
+			//범위 초과면 에러
+			if(atoi(lexeme) < low || high < atoi(lexeme))
+				return false;
+			
+			buf[buflen++] = atoi(lexeme);
+		}
+		//'-'체크
+		else if(cycle[i] == '-') 
+			buf[buflen++] = BAR;
+
+		//'/'체크
+		else if(cycle[i] == '/') 
+			buf[buflen++] = DIVIDE;
+
+		//'*' 체크
+		else if(cycle[i] == '*') 
+			buf[buflen++] = STAR;
+
+		//',' 체크
+		else if(cycle[i] == ',') 
+			buf[buflen++] = COMMA;
+	}
+
+	for(i = 0; i < buflen; i++) {
+		//'*'일 경우
+		if(buf[i] == STAR) {
+			//마지막이거나
+			if(i+1 == buflen)
+				return true;
+			else if (i+2 < buflen) {
+				//뒤에 마지막이 아닌 ','이 오거나
+				if(buf[i+1] == COMMA)
+					i +=1;	
+				//마지막인 '/'와 숫자 또는 그 뒤에 마지막이 아닌 ','가 오지 않으면 에러
+				else if(buf[i+1] == DIVIDE && buf[i+2] > 0) {
+					if(i+3 == buflen)
+						return true;
+					else if(i+4 < buflen && i+3 == COMMA)
+						i+= 3;
+					//그외 에러
+					else
+						return false;
+				}
+			}
+		}
+		//숫자일 경우
+		else if(buf[i] > 0) {
+			//마지막이거나
+			if(i+1 == buflen)
+				return true;
+			else if (i+2 < buflen) {
+				//뒤에 마지막이 아닌 ','이 오거나
+				if(buf[i+1] == COMMA)
+					i +=1;	
+				//뒤에 '-'와 숫자면
+				else if(buf[i+1] == BAR && buf[i+2] > 0) {
+					//마지막이거나
+					if(i+3 == buflen)
+						return true;
+					else if(i+4 < buflen) {
+						//마지막 주기거나
+						if(i+5 == buflen && buf[i+3] == DIVIDE && buf[i+4] > 0) 
+							return true;
+						//그 뒤에 마지막이 아닌 ','가 있으면
+						else if (i+5 < buflen && buf[i+5] == COMMA)
+							i += 5;
+						//그 외 에러
+						else
+							return false;
+					}
+				}
+				//그외 에러
+				else 
+					return false;
+			}
+		}
+		//그 외 에러
+		else
+			return false;
+	}
+
+	return true;
+}
+
 
 void write_log(char *msg, char *flag) //현재 시간, flag(run, add) msg를 로그에 기록
 {
--- ssu_crontab.h
+++ ssu_crontab.h
@@ -1,11 +1,27 @@
 #ifndef SSU_CRONTAB_H_
 #define SSU_CRONTAB_H_
 
+#ifndef STAR 
+	#define STAR -1 
+#endif
+#ifndef BAR 
+	#define BAR -2 
+#endif
+#ifndef DIVIDE
+	#define DIVIDE -3 
+#endif
+#ifndef COMMA
+	#define DIVIDE -4 
+#endif
+
+void ssu_runtime(void); //프로그램 수행시간 출력함수
+
 void print_prompt(); //프롬프트 출력
 int execute_command(char *command); //명령어 구분하여 각각의 명령어 실행함수 호출, exit시 -1리턴
 
 void cmd_add(char *cmd); //add명령어 실행
 int check_add_cmd(char *cmd); //인자 검사 오류시 false리턴
+int check_cycle(int low, int high, char *cycle); //cycle이 low, high에 존재하고, 형식에 이상 없으면 true
 void write_log(char *msg, char *flag); //현재 시간, flag(run, add) msg를 로그에 기록
 
 void cmd_remove(char *cmd); //remove 명령어 실행
